35b338d286104aabaf9381a65b6886dfce65d7a6
Shota Sato <last11142@gmail.com>
2023-09-10 23:46:10 +0900

    complete

--- src/button.tsx
+++ src/button.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react"
+
+type ButtonColor = "red" | "blue" | "green"
+
 export const Button = () => {
+  useEffect(() => {
+    const previousButonColor = localStorage.getItem("buttonColor") as ButtonColor  // 暗黙の型を扱うときには明示的に型を示そう
+  }, []);
+
   return <button>Click me!</button>
 }
